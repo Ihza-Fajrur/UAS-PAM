e730685782b0a03d9e83b55853410505f1c4f6f2
Ihza-Fajrur <ihza.119140130@student.itera.ac.id>
2022-05-28 01:08:56 +0700

    API data retrieval

--- Reviewin/screens/ListBarang.js
+++ Reviewin/screens/ListBarang.js
@@ -1,9 +1,19 @@
-import React, { useState, Component } from 'react';
+import React, { useState, Component, useEffect } from 'react';
 import styles from '../styles/style.js';
 import { Text, View, Button, TextInput, SafeAreaView, TouchableOpacity,ScrollView, Modal, FlatList, Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 export default function ListBarang({navigation}) {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('http://192.168.1.2:19002/products_list',{
+            method:'GET'
+        })
+        .then((res)=> res.json())
+        .then((products)=>{
+            setProducts(products) 
+        })
+    }, [])
     return (
         // <Navigator/>
         <SafeAreaView style={styles.container}>
@@ -29,55 +39,28 @@ export default function ListBarang({navigation}) {
             <View style={styles.itemPhoto}>
                 <Image source={require('../assets/images/macbook-pro-13-og-202011.jpg')} style={{width: "100%", height: 200, borderTopRightRadius:10, borderTopLeftRadius:10,}}/>
             </View>
-            <View style={styles.itemDescription}>
-                <Text style={[{fontSize:20, color:"black" }]}>
-                MACBOOK RETINA 256GB
-                </Text>
-                <Text style={[{color:"#EC9948", fontWeight:"bold"}]}>
-                Rp. 17.000.000
-                </Text>
-                <View style={[{justifyContent: 'space-between', flexDirection:"row"}]}>
-                <View style={[{flexDirection:"row",justifyContent: 'space-between', marginTop:5}]}>
-                <FontAwesome5 name="star" size={20} color="black" />
-                    <Text style={[{fontSize:14, fontWeight:"bold", marginLeft:5}]}>
-                    5.0
-                    </Text>
-                </View>
-                <View style={[{alignItems:"flex-end"}]}>
-                    <TouchableOpacity style={[{alignSelf:"flex-end", flexDirection:"row"}]} onPress={()=>navigation.navigate('DetailProduk')}>
-                    <Text>
-                        Detail
-                    </Text>
-                    <AntDesign name="arrowright" size={20} color="black" />
-                    </TouchableOpacity>
-                </View>
-                </View>
-            </View>
             
-            </View>
-            {/* End of item card */}
-
-            {/* Item Card */}
-            <View style={styles.item}>
-            <View style={styles.itemPhoto}>
-                <Image source={require('../assets/images/macbook-pro-13-og-202011.jpg')} style={{width: "100%", height: 200, borderTopRightRadius:10, borderTopLeftRadius:10,}}/>
-            </View>
             <View style={styles.itemDescription}>
-                <Text style={[{fontSize:20, color:"black" }]}>
-                MACBOOK RETINA 256GB
+                <Text style={[{fontSize:20, color:"black" }]} >
+                    {products.map(product => product.nama)}
                 </Text>
                 <Text style={[{color:"#EC9948", fontWeight:"bold"}]}>
-                Rp. 17.000.000
+                    Rp.{products.map(product => product.harga)}
                 </Text>
                 <View style={[{justifyContent: 'space-between', flexDirection:"row"}]}>
                 <View style={[{flexDirection:"row",justifyContent: 'space-between', marginTop:5}]}>
-                <FontAwesome5 name="star" size={20} color="black" />
+                <AntDesign name="star" size={24} color="#FFCD3C" />
                     <Text style={[{fontSize:14, fontWeight:"bold", marginLeft:5}]}>
-                    5.0
+                    {products.map(product => product.rating)}
                     </Text>
                 </View>
                 <View style={[{alignItems:"flex-end"}]}>
-                    <TouchableOpacity style={[{alignSelf:"flex-end", flexDirection:"row"}]}>
+                    <TouchableOpacity style={[{alignSelf:"flex-end", flexDirection:"row"}]} onPress={()=>navigation.navigate('DetailProduk', {
+                        nama: products.map(product => product.nama),
+                        harga: products.map(product => product.harga),
+                        rating: products.map(product => product.rating),
+                        spesifikasi: products.map(product => product.spesifikasi)
+                    })}>
                     <Text>
                         Detail
                     </Text>
